2907ceb225d0c48f7b2fa13fd84b90da8a73eb90
balbarka <balbarka>
2022-10-07 14:52:36 +0000

    eval update

--- example_ref_import/entry_nb.py
+++ example_ref_import/entry_nb.py
@@ -1,6 +1,24 @@
 # Databricks notebook source
-from ..demo import App
+import os
+os.getcwd()
+
+# COMMAND ----------
+
+import sys
+sys.path
+
+# COMMAND ----------
+
+from app import App
 
 # COMMAND ----------
 
 app = App()
+
+# COMMAND ----------
+
+app.x
+
+# COMMAND ----------
+
+
